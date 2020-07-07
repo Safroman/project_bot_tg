bf23f2d4cdfb3a7f616928e02412184289c9152b
Safroman <Safoncheg@gail.com>
2020-07-08 01:00:43 +0300

    bla

--- main.py
+++ main.py
@@ -10,32 +10,32 @@ from webshop.production import VERSION
 app = Flask(__name__)
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    if VERSION == 'production':
-        # api_app.run(debug=True)
+if VERSION == 'production':
+    # api_app.run(debug=True)
 
-        @app.route(config.WEBHOOK_PATH, methods=['POST'])
-        def webhook():
-            if request.headers.get('content-type') == 'application/json':
-                json_string = request.get_data().decode('utf-8')
-                update = Update.de_json(json_string)
-                bot.process_new_updates([update])
-                return ''
-            else:
-                abort(403)
+    @app.route(config.WEBHOOK_PATH, methods=['POST'])
+    def webhook():
+        if request.headers.get('content-type') == 'application/json':
+            json_string = request.get_data().decode('utf-8')
+            update = Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            abort(403)
 
-        bot.remove_webhook()
-        time.sleep(1)
+    bot.remove_webhook()
+    time.sleep(1)
 
-        bot.set_webhook(config.WEBHOOK_URL,
-                        certificate=open('webhook_cert.pem', 'r')
-                       )
+    bot.set_webhook(config.WEBHOOK_URL,
+                    certificate=open('webhook_cert.pem', 'r')
+                   )
 
-        app.run(debug=True)
+    app.run(debug=True)
 
-    else:
-        bot.remove_webhook()
-        start_bot()
-        bot.polling()
-        # app.run(debug=True)
+else:
+    bot.remove_webhook()
+    start_bot()
+    bot.polling()
+    # app.run(debug=True)
