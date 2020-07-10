b23d4b3c1f43c394c0cee3a56e1578f62f3ca77d
Safroman <Safoncheg@gail.com>
2020-07-10 20:04:11 +0300

    drop db

--- webshop/db/seeder.py
+++ webshop/db/seeder.py
@@ -1,4 +1,4 @@
-from models import import TITLES, Text, Category, Product, Attrs
+from models import TITLES, Text, Category, Product, Attrs
 from random import randint, choice
 import mongoengine as me
 
