36e70cc067d015dee3bd1d05fa189e168f9c102c
Safroman <Safoncheg@gail.com>
2020-07-10 18:58:44 +0300

    prod version

--- webshop/bot/lookups.py
+++ webshop/bot/lookups.py
@@ -1,5 +1,3 @@
-
-
 category_lookup = 'category'
 root_lookup = 'root'
 product_lookup = 'product'
@@ -14,32 +12,3 @@ cart_checkout = 'checkout'
 
 discount_lookup = 'discount'
 back_lookup = ''
-
-bot_map = {
-    'Товары со скидкой': discount_lookup,
-    category_lookup: 'category',
-    cart_lookup: {
-        cart_plus: 'plus',
-        cart_minus: 'minus',
-        cart_remove: 'remove',
-        cart_checkout: 'checkout'
-    }
-}
-
-
-def navigator(message=None, call=None):
-    if message:
-        data = [message.text, ]
-    elif call:
-        data = call.data.split(separator)
-
-    var = bot_map
-
-    for lookup in data:
-        var = var[lookup]
-        print(var)
-    return var
-
-
-if __name__ == '__main__':
-    print(bot_map)
